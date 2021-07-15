5375f0b79cf9c1bc0625ba5475ea6fbbfefc4254
Miguel Brandão <mickael.a.brandao@gmail.com>
2021-07-15 16:59:06 +0100

    Add output in case there are no notifications

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ headers = {"Authorization": "token "+secrets.token,
            "Accept": "application/vnd.github.v3+json"}
 params = {"all": "false"}  # only unread notifications
 
+icon = "--icon=/home/miguel/dev/notification-baby-monitor/github_icon_32px.png"
+expire = "--expire-time=20000"
+
 raw_response = requests.get(endpoint, headers=headers, params=params)
 
 if raw_response.status_code == requests.codes.ok:
@@ -19,11 +22,11 @@ if raw_response.status_code == requests.codes.ok:
             notification_titles.append(
                 "  • [" + notification["repository"]["name"] + "] " + notification["subject"]["title"])
         title = "There are " + str(n_notifications) + " new notifications:"
-        icon = "--icon=/home/miguel/dados/dev/notification-baby-monitor/github_icon_32px.png"
-        arguments = ["/usr/bin/notify-send", icon,
+        arguments = ["/usr/bin/notify-send", expire, icon,
                      title, "\n".join(notification_titles)]
         subprocess.Popen(arguments)
     else:
-        pass
+        arguments = ["/usr/bin/notify-send", expire, icon, "Everything clear!"]
+        subprocess.Popen(arguments)
 else:
     print("Request returned code" + raw_response.status_code)
